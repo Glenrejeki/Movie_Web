eb9dbaca697814cd4123b4da208892a238b4ef00
Jeki <142556335+Glenrejeki@users.noreply.github.com>
2025-06-18 12:17:04 +0700

    Operator perbandingan

---
+++ module-4-operator/scriptOperator.js
@@ -0,0 +1,40 @@
+// == Mengecek nilai
+
+let a = "10"
+let b = 10 
+console.log (a==b)
+
+// === Mengecek nilai dan tipe data
+let c = "10" // string
+let d = 10 // number
+console.log(c === d )
+
+// != mengecek ketidaksamaan nilai
+let e = "5"
+let f = 10
+console.log( e != f)
+
+// !== mengecek ketidaksamaan nilai dan tipe data
+let g = "5"
+let h = 10
+console.log(g !== h )
+
+// > cek lebih besar 
+let i = 1
+let j = 2
+console.log(i > j)
+
+// < cek lebih kecil
+let k = 3
+let l = 4
+console.log( k < l)
+
+// >= cek lebih besar atau sama dengan
+let m = 0
+let n = 1
+console.log(m>= n )
+
+// <= cek lebih kecil atau sama dengan
+let o = 5
+let p = 5
+console.log(o <= p )
