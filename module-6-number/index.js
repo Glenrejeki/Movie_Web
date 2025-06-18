ce93a68ca4b41cc419fcd77a8b2f1ab4e095c282
Jeki <142556335+Glenrejeki@users.noreply.github.com>
2025-06-18 21:29:38 +0700

    Number belum selesai

---
+++ module-6-number/index.js
@@ -0,0 +1,11 @@
+let bilBulat = 12
+let bilPecahan = 4.12
+let bilNegatif = -1
+let infinityValue = Infinity
+let notANumber = NaN
+
+console.log(bilBulat)
+console.log(bilPecahan)
+console.log(bilNegatif)
+console.log(infinityValue)
+console.log(notANumber)
