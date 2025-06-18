de1caf67eca7897409720d8580cab36ecc68456b
Jeki <142556335+Glenrejeki@users.noreply.github.com>
2025-06-18 11:52:51 +0700

    aritmatika done

---
+++ module-4-operator/index.js
@@ -0,0 +1,49 @@
+// Operator aritmatika
+
+let x = 10 
+let y = 5
+let sum = x + y
+console.log("Penjumlahan:", sum, "Tipe :" , typeof sum)
+
+
+//Pengurangan
+let a= 30
+let b= 10
+let difference = x - y
+console.log("Pengurangan : ", difference, "Tipe :", typeof difference)
+
+
+// Perkalian
+let p = 3
+let q = 3
+let Perkalian = p * q
+console.log("Perkalian : ", Perkalian, "Tipe :", typeof Perkalian )
+
+// Pembagian
+let m = 0
+let n = 1
+let divi = m/n
+console.log ("Pembagian : ", divi, "Tipe :", typeof divi)
+
+// Modulus
+let r = 15
+let s = 4
+let modu = r % s 
+console.log("Modulus : ", modu, "Tipe :", typeof modu)
+
+//Eksponen
+let basis = 2 
+let pangkat = 3
+let eksponen = basis ** pangkat
+console.log("Eksponen : ", eksponen, "Tipe :", typeof eksponen)
+
+// Incremenet 
+let count =  10
+count ++ 
+console.log("Incement :", count, "Tipe :", typeof count)
+
+
+// Decrement 
+let countDown = 3
+countDown--
+console.log ("Decrement :" , countDown, "Tipe :", typeof countDown)
