17166230be846bd25d9f55ac4ea025ecafee0cc7
Jeki <142556335+Glenrejeki@users.noreply.github.com>
2025-06-18 12:22:40 +0700

    Operator Logic DONE

---
+++ module-4-operator/scriptLogic.js
@@ -0,0 +1,14 @@
+// And Operator Logic
+let a = true
+let b = false
+console.log (a && b)
+
+// Or Operator Logic
+let c = true
+let d = false 
+console.log ( c || d)
+
+// Not Operator Logic
+let e = true
+let notResult = !e
+console.log (notResult)
