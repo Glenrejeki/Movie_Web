33f049929583ac8462bf9321e72d239027ad9c14
Jeki <142556335+Glenrejeki@users.noreply.github.com>
2025-06-18 11:39:27 +0700

    Type Conversion done

--- module-3-type-conversion/index.js
+++ module-3-type-conversion/index.js
@@ -4,12 +4,20 @@
 
 // Eksplisit -> Dilakukan secara manual oleh programmer
 
+// IMPLISIT
+
 let result = 10  + "20"
 let result2 = 10 * "20"
 let result3 = 10  - "20"
 console.log(result, typeof result)
 console.log(result2, typeof result2)
 console.log(result3, typeof result3)
+
+// Hati denngan Nan dengan number 
+let hasil = "abc" + 10
+let hasil2 = "abc" - 10
+console.log("Hasil String dengan number jika penjumlahan:",hasil, typeof hasil)
+console.log("Hasil String dengan number jika bukan penjumlahan :",hasil2, typeof hasil2)
 
 // falsy value -> Nilai yang dianggap false dalam konteks boolean (0, "", null, undefined, NaN  )
 let boolean = !0
@@ -33,3 +41,8 @@ let num3 = parseFloat(str3)
 console.log(str, typeof str)
 console.log(str2, typeof str2)
 console.log(num3, typeof num3)
+
+let bool = Boolean(1)
+let bool2 = Boolean(0)
+console.log(bool, typeof bool)
+console.log(bool2, typeof bool2)
