32249d4209bd2b4893d9912d1146d3e388fa628f
Jeki <142556335+Glenrejeki@users.noreply.github.com>
2025-06-18 12:02:43 +0700

    Operator Assigment done

---
+++ module-4-operator/scriptAssiment.js
@@ -0,0 +1,30 @@
+// ASSIGNMENT OPERATORS
+
+// Dasar
+let nama = "Jeki"
+console.log("Nama :",nama)
+
+// Penjumlahan 
+let x = 1
+x += 2
+console.log("Penjumlahan : ", x)
+
+// Pengurangan
+let y = 4
+y -= 2
+console.log("Pengurangan : ", y)
+
+// Perkalian
+let z = 3
+z *=10
+console.log("Perkalian :",z)
+
+//Pembagian
+let a = 20
+a /= 4
+console.log("Pembagian :", a)
+
+// Modulo
+let b = 13
+b%= 3
+console.log("Modulo : ", b)
