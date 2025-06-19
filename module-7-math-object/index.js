5e3969b0661cc495d18b9026e0e84da384629fdf
Jeki <142556335+Glenrejeki@users.noreply.github.com>
2025-06-19 15:58:33 +0700

    math objek done

---
+++ module-7-math-object/index.js
@@ -0,0 +1,19 @@
+console.log(Math.PI);
+console.log(Math.E);
+
+// Method math
+console.log(Math.abs(-11));
+console.log(Math.pow(3, 2));
+console.log(Math.sqrt(25));
+console.log(Math.cbrt(8));
+console.log(Math.max(1, 2, 90, 103, 40, 20, -1000, 700));
+console.log(Math.min(1, 2, 90, 103, 40, 20, -1000, 700));
+
+// Pembulatan angka
+console.log(Math.round(3.5));
+console.log(Math.ceil(4.1)); // selalu membulatkan ke atas
+console.log(Math.floor(2.9)); //selalu membulatkan ke bawah
+console.log(Math.trunc(9.9)); // menghilangkan angka dibelakang koma
+
+// Random Number
+console.log(Math.round(Math.random() * 10));
