ea3f21369495cc2e45da257b031c29402c1d1980
Jeki <142556335+Glenrejeki@users.noreply.github.com>
2025-07-07 21:41:37 +0700

    Carousel Image Slider Not Done
    1. JS haven't finish
    2. I think change the picture, so the size of picture can be same / similiar

---
+++ proyek-5-Carousel-Image-Slider/script.js
@@ -0,0 +1,12 @@
+  // Get Element HTML
+  const carouselSlide= document.querySelector(`.carousel-slide`) // contariner for all image
+   
+  const carouselImages = document.querySelectorAll(`.carousel-slide img`)
+
+  const prevBtn = document.querySelector(`.prev-btn`)
+  const nextBtn = document.querySelector(`.next-btn`)
+
+  const indicators = document.querySelectorAll(`.indicator`))
+
+
+
