a02803451e93550292ec5d4dbf05873a584dacf3
Jeki <142556335+Glenrejeki@users.noreply.github.com>
2025-07-18 21:37:12 +0700

    Faq done bos ku

--- proyek-7-FAQ-Accordion/script.js
+++ proyek-7-FAQ-Accordion/script.js
@@ -14,14 +14,72 @@ const faqData = [
   }
 ];
 
+// Get accordion container 
+const accordionContainer = document.getElementById(`accordion`)
+
 
 // Create function to generate faqData from array faqData 
 
 function generateAccordionItems(faqData){
   faqData.forEach(item => {
     const accordionItem = document.createElement(`div`)
+    accordionItem.classList.add(`accordion-item`)
 
 
-    accordionItem.classList(`accordion-item`)
+    //Create element for header 
+    const header = document.createElement('button')
+    header.classList.add('accordion-header')
+    header.textContent = item.question
+
+    // Create element for content 
+    const content = document.createElement(`div`)
+    content.classList.add(`accordion-content`)
+
+
+    //Create element for paragraph 
+    const contentText= document.createElement(`p`)
+    contentText.textContent = item.answer
+
+    // Insert element to html
+    content.appendChild(contentText)
+    accordionItem.appendChild(header)
+    accordionItem.appendChild(content)
+
+    // Add accordion item to accordion container 
+    accordionContainer.appendChild(accordionItem)
+
   });
 }
+
+
+// call function generate faq
+generateAccordionItems(faqData)
+
+
+
+// get element acordion header 
+const accordionHeaders = document.querySelectorAll('.accordion-header')
+
+
+// Add EventListener
+accordionHeaders.forEach(header => {
+  header.addEventListener(`click`, ()=>{
+    header.classList.toggle('active')
+
+    const accordionContent = header.nextElementSibling
+
+    if(header.classList.contains('active')){
+      accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px'
+
+      }else {
+        accordionContent.style.maxHeight =0
+      }
+
+    accordionHeaders.forEach(otherHeader => {
+      if(otherHeader != header && otherHeader.classList.contains('active')){
+        otherHeader.classList.remove('active')
+        otherHeader.nextElementSibling.style.maxHeight= 0
+      }
+    })
+})
+})
